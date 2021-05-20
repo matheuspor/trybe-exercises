2302e26913a9dd98031b76b963ac3c6b64bc06ce
Matheus <matheus.pormanhani@gmail.com>
2021-05-20 18:09:01 -0300

    Adiciona exercício 4

---
+++ exercises/bloco_4-Javascript/dia_2/script4.js
@@ -0,0 +1,19 @@
+// Exercício 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+
+let mediaArit = soma / numbers.length;
+
+if (mediaArit > 20) {
+  console.log("valor maior que 20")
+} else {
+  console.log("valor menor ou igual a 20")
+}
+
+
+//
