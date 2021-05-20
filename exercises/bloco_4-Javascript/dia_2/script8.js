3fdc4ec84effa937950064c8716dc7c8f42b92ce
Matheus <matheus.pormanhani@gmail.com>
2021-05-20 19:02:42 -0300

    adiciona exercício 8

---
+++ exercises/bloco_4-Javascript/dia_2/script8.js
@@ -0,0 +1,12 @@
+// Exercício 8
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let array = [];
+
+for (let index = 0; index <= 20; index += 1) {
+  array.push(index);
+}
+
+console.log(array)
+
+//
