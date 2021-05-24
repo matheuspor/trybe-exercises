640a906e4aeabf889371503f18b258554b64d459
Matheus <matheus.pormanhani@gmail.com>
2021-05-24 18:14:54 -0300

    Resolução do exercício parte 1.5

--- exercises/bloco_4-Javascript/dia_4/script1.js
+++ exercises/bloco_4-Javascript/dia_4/script1.js
@@ -5,6 +5,17 @@ let info = {
   recorrente: 'Sim',
 };
 
-for (let key in info) {
-  console.log(info[key]);
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info, info2) {
+  if (info[key] === info2[key]) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
 }
