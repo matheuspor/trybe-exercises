27a8067b8e54c98f7ad3ed1585ff289053507df5
Matheus <matheus.pormanhani@gmail.com>
2021-05-24 19:15:59 -0300

    Resolução do exercício parte 2.3

--- exercises/bloco_4-Javascript/dia_4/script2.2.js
+++ exercises/bloco_4-Javascript/dia_4/script2.2.js
@@ -1,11 +1,11 @@
-let testArray = [2, 4, 6, 7, 10, 0, -3, 20];
+let testArray = [2, 4, 6, 7, 10, 0, -3, 20,];
 
 
 function maxArray(array) {
   let numberMaxKey = 0;
 
   for (let key in array) {
-    if (array[numberMaxKey] < array[key]) {
+    if (array[numberMaxKey] > array[key]) {
       numberMaxKey = key;
     }
   }
