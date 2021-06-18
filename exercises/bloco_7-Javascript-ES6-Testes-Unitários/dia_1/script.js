270d2549d13eef50e7240982c4570bb580af5844
Matheus <matheus.pormanhani@gmail.com>
2021-06-18 11:27:40 -0300

    Resolução exercício 1 parte 2

--- exercises/bloco_7-Javascript-ES6-Testes-Unitários/dia_1/script.js
+++ exercises/bloco_7-Javascript-ES6-Testes-Unitários/dia_1/script.js
@@ -1,9 +1,3 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const factorial = num => num > 1 ? num * factorial(num - 1) : 1;
 
-const compare = (numberA, numberB) => {
-  return numberA - numberB;
-}
-
-console.log(`Os números ${oddsAndEvens.sort(compare)} se encontram ordenados em ordem crescente`);
-
-// source: https://www.w3schools.com/jsref/jsref_sort.asp
+console.log(factorial(3));
