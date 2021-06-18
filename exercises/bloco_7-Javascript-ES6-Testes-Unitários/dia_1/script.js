dd56a88dccf943406d9a172471e05bf41cd7ebc4
Matheus <matheus.pormanhani@gmail.com>
2021-06-18 08:15:25 -0300

    Resolução exercício 2 parte 1

--- exercises/bloco_7-Javascript-ES6-Testes-Unitários/dia_1/script.js
+++ exercises/bloco_7-Javascript-ES6-Testes-Unitários/dia_1/script.js
@@ -1,12 +1,9 @@
-const testingScope = escopo => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const compare = (numberA, numberB) => {
+  return numberA - numberB;
 }
 
-testingScope(true);
+console.log(`Os números ${oddsAndEvens.sort(compare)} se encontram ordenados em ordem crescente`);
+
+// source: https://www.w3schools.com/jsref/jsref_sort.asp
