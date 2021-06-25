4a019868c0aa8d51c032a267b14b0b0b41fc271d
Matheus <matheus.pormanhani@gmail.com>
2021-06-24 22:32:45 -0300

    exercício 1

--- exercises/bloco_8-Javascript-HOFs-ES6/dia_1/script.js
+++ exercises/bloco_8-Javascript-HOFs-ES6/dia_1/script.js
@@ -1,18 +1,13 @@
-const personId = (nome) => {
-  const person = {
-    nomeCompleto: nome,
-    email: `${nome.replace(/ /g, "_")}@trybe.com`
-  }
-  return person
-};
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const newEmployees = () => {
-  const employees = {
-    id1: personId('João Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: personId('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: personId('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+const result = (rightAnswers, studentAnswers) => {
+  let count = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    if (rightAnswers[index] === studentAnswers[index])
+      count += 1;
   }
-  return employees;
-};
+  console.log(count);
+}
 
-console.log(newEmployees(personId));
+const checkAnswers = (rightAnswers, studentAnswers, result) => result;
