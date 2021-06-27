b900ed3d9ab3eca4f71e2f2002e7b9635b9b1031
Matheus <matheus.pormanhani@gmail.com>
2021-06-27 14:25:48 -0300

    exercício 2

--- exercises/bloco_8-Javascript-HOFs-ES6/dia_3/exercises.js
+++ exercises/bloco_8-Javascript-HOFs-ES6/dia_3/exercises.js
@@ -64,16 +64,44 @@ const books = [
 ];
 
 const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
 ];
 
-function formatedBookNames(booksobject) {
-  return booksobject.map((book) => (`${book.name} - ${book.genre} - ${book.author.name}`));
+function nameAndAge(bookArray) {
+  const bookobject = bookArray.map((book) => {
+    return {
+      ['age']: book.releaseYear - book.author.birthYear,
+      ['author']: book.author.name
+    }
+  })
+  return bookobject.sort((a, b) => {
+    return a.age - b.age;
+  })
 }
 
-assert.deepStrictEqual(formatedBookNames(books), expectedResult);
+assert.deepStrictEqual(nameAndAge(books), expectedResult);
+
+console.log(nameAndAge(books));
