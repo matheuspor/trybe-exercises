818ae0664ab7de1c0d56c3227620cb0285f3288c
Matheus <matheus.pormanhani@gmail.com>
2021-06-29 09:09:35 -0300

    exercício 2

--- exercises/bloco_8-Javascript-HOFs-ES6/dia_4/script.js
+++ exercises/bloco_8-Javascript-HOFs-ES6/dia_4/script.js
@@ -1,13 +1,75 @@
 const assert = require('assert');
 
-const arrays = [
-  ['1', '2', '3'],
-  [true],
-  [4, 5, 6],
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
 ];
 
-function flatten() {
-  return arrays.reduce((acc, current) => acc.concat(current));
+// Adicione o código do exercício aqui:
+
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames() {
+  const string = books.reduce((acc, curr) => `${acc}${curr.author.name}, `, '').slice(0, -2) + `.`;
+  return string;
 }
 
-assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+assert.strictEqual(reduceNames(), expectedResult);
