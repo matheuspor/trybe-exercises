68f9f8c31dcf85f30b138177cf8b624354e08c9b
Matheus <matheus.pormanhani@gmail.com>
2021-06-29 20:34:59 -0300

    exercício 4

--- exercises/bloco_8-Javascript-HOFs-ES6/dia_5/script.js
+++ exercises/bloco_8-Javascript-HOFs-ES6/dia_5/script.js
@@ -1,21 +1,38 @@
 const assert = require('assert');
 
-const alex = {
-  name: 'Alex',
-  age: 26,
-  likes: ['fly fishing'],
-  nationality: 'Australian',
-};
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
 
-const gunnar = {
-  name: 'Gunnar',
-  age: 30,
-  likes: ['hiking', 'scuba diving', 'taking pictures'],
-  nationality: 'Icelandic',
-};
+// escreva filterPeople abaixo
+const filterPeople = (obj) => {
+  return obj.filter(({ bornIn, nationality }) => bornIn > 1900 && bornIn < 2000 && nationality === 'Australian');
+}
+const filteredPeople = filterPeople(people);
 
-// complete a assinatura da função abaixo 
-const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
-
-assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
-assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
