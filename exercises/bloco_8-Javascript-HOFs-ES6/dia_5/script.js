afbfacff772e2edb973bc1839610e0954e2a228e
Matheus <matheus.pormanhani@gmail.com>
2021-06-29 20:42:38 -0300

    exercício 5

--- exercises/bloco_8-Javascript-HOFs-ES6/dia_5/script.js
+++ exercises/bloco_8-Javascript-HOFs-ES6/dia_5/script.js
@@ -1,38 +1,11 @@
 const assert = require('assert');
 
-const people = [
-  {
-    name: 'Nicole',
-    bornIn: 1992,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Harry',
-    bornIn: 2008,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Toby',
-    bornIn: 1901,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Frida',
-    bornIn: 1960,
-    nationality: 'Dannish',
-  },
-  {
-    name: 'Fernando',
-    bornIn: 2001,
-    nationality: 'Brazilian',
-  },
-];
+const myList = [1, 2, 3];
 
-// escreva filterPeople abaixo
-const filterPeople = (obj) => {
-  return obj.filter(({ bornIn, nationality }) => bornIn > 1900 && bornIn < 2000 && nationality === 'Australian');
-}
-const filteredPeople = filterPeople(people);
+const swap = ([first, second, third]) => [third, second, first];
 
-assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
-assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
+const swappedList = swap(myList);
+
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);
