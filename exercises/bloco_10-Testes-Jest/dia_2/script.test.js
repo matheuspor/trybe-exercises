419aa7f3c001436083e54a1da7cc6a27f98c3486
Matheus <matheus.pormanhani@gmail.com>
2021-07-15 10:21:39 -0300

    Resolução exercício 3

--- exercises/bloco_10-Testes-Jest/dia_2/script.test.js
+++ exercises/bloco_10-Testes-Jest/dia_2/script.test.js
@@ -1,15 +1,15 @@
 const { expect } = require('@jest/globals');
 const getUserName = require('./script');
 
-test('If User find', () => {
-  return getUserName(4)
+test('If User find', async () => {
+  await getUserName(4)
     .then(user => {
       expect(user).toBe('Mark');
     })
 })
 
-test('If User not found', () => {
-  return getUserName(1)
+test('If User not found', async () => {
+  await getUserName(1)
     .catch(error => {
       expect(error).toEqual({ error: 'User with 1 not found.'});
     })
